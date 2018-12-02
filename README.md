b1d87785507852c50ef735b7696f1a56122b6fcf
pepa65 <pepa65@passchier.net>
2018-12-02 01:38:05 -0800

    Rewite of readme; add log parameter to make quiet

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
 # deadman
-Deadman is an anti-forensic linux kernel module deadman-switch that responds to usb events. By default it turns the computer off when a specific USB device is removed. It can also respond to any USB device being plugged in.
-
-Inspired by [silk-guardian](https://github.com/pepa65/silk-guardian.git), but much simpler in scope.
+Deadman is linux kernel module deadman-switch that responds to removal of a specific USB device (or the insertion of any).
 
 ## Build & Use
 * Build: `make`
 * Install: `sudo make install`
-* Usage: `sudo insmod deadman.ko [id=0xVENDPROD] [off=0] [ins=1]`
-* Usage (if installed): `sudo modprobe deadman [id=0xVENDPROD] [off=0] [ins=1]`
+* Usage: `sudo insmod <path>/deadman.ko [id=0xVENDPROD] [off=0] [ins=1] [log=0]`
+* Usage (if installed): `sudo modprobe deadman [id=0xVENDPROD] [off=0] [ins=1] [log=0]`
   - `id`: *VEND* is the 2-byte hexadecimal code for idVendor and *PROD* for idProduct. The `id` parameter can also be set to a default device in the source before building.
-  - `ins`: if ins=1 is passed, any USB device getting inserted triggers a panic.
-  - `off`: if `off=0` is passed, the computer will not turn off at a panic.
+  - `off`: if `off=0` is given, the computer will not turn off at a panic.
+  - `ins`: if ins=1 is given, any USB device getting inserted triggers a panic.
+  - `log`: if `log=0` is given, no messages will be logged anywhere.
 
-## Why?
-When the drives your machine are fully (or partially) encrypted, they are still vulnerable when in-use, unencrypted. Requiring the specified USB device to remain connected is an added security feature; once the system is down, all encrypted data is only accessible by decrypting it first.
-
-When a USB device is strapped to ones wrist, it will be harder for third parties to prevent the device to be removed when attacked.
-
-### Other tools
-Silk-guardian has broader uses, it can also shred certain files (in case no encryption is in use), it can respond to unknown USB devices (such as a "mouse jiggler" as used by law enforcement to keep the system from sleeping, or a USB drive that someone is wanting to copy files to).
+## Rationale
+When information on the computer is encrypted, it is still accessible when in use, unencrypted. Only when turned off does the encryption fully protect the information, and when a computer is seized by a competent agent while running, unencrypted information can be accessed. If the computer can be made to turn off in an emergency, it will heightens the security. A deadman USB device only needs to be removed to trigger a power-off, and when it is strapped to ones wrist, it will be harder to prevent the removal when attacked. Another attack is insertion of a USB device into a running system, and this can also be made to trigger a power-off.
--- deadman.c
+++ deadman.c
@@ -11,38 +11,43 @@ MODULE_DESCRIPTION("Panic on removal of 'deadman' USB device or any USB device b
 MODULE_PARM_DESC(id, "USB ID of the deadman device: 'id=0x1234abcd' (vendor 0x1234, product 0xabcd)");
 MODULE_PARM_DESC(ins, "Panic on any USB device being inserted: 'ins=1'");
 MODULE_PARM_DESC(off, "Don't turn the computer off on panic: 'off=0'");
+MODULE_PARM_DESC(log, "No deadman messages in any logs: 'log=0'");
 
-// Default USB device ID of trigger
+// Default USB device ID of trigger; override: id=0x1234abcd
 static unsigned int id = 0xabcd1234;
 module_param(id, int, 0644);
 
-// By default inserted USB devices don't triggers a panic, override: ins=1
+// Default: inserted USB devices don't trigger panic; override: ins=1
 static int ins = 0;
 module_param(ins, int, 0644);
 
-// By default the computer turns off on panic, override: off=0
+// Default: computer turns off on panic; override: off=0
 static int off = 1;
 module_param(off, int, 0644);
+
+// Default: messages logged on (de)activation and triggering, override: log=0
+static int log = 1;
+module_param(log, int, 0644);
 
 static void start_panic(struct usb_device *dev){
 	struct device *mark;
 	for (mark = &dev->dev; mark; mark = mark->parent) mutex_unlock(&mark->mutex);
 	if (off){
-		printk("Powering off\n");
+		if (log) printk("Powering off\n");
 		kernel_power_off();
 	}
-	pr_info("Not powering off\n");
+	if (log) pr_info("Not powering off\n");
 }
 
 static void usb_inserted(struct usb_device *dev){
 	unsigned int d = 0x10000*dev->descriptor.idVendor+dev->descriptor.idProduct;
-	pr_info("USB device 0x%08x inserted\n", d);
+	if (log) pr_info("USB device 0x%08x inserted\n", d);
 	start_panic(dev);
 }
 
 static int usb_removed(struct usb_device *dev){
 	unsigned int d = 0x10000*dev->descriptor.idVendor+dev->descriptor.idProduct;
-	pr_info("USB device 0x%08x removed\n", d);
+	if (log) pr_info("USB device 0x%08x removed\n", d);
 	if (d == id) start_panic(dev);
 	return 0;
 }
@@ -56,17 +61,21 @@ static struct notifier_block usb_notify = {.notifier_call = notify};
 
 static int __init deadman_init(void){
 	usb_register_notify(&usb_notify);
-	if (id) pr_info("Watching removal of USB device 0x%08x\n", id);
-	if (ins) pr_info("Watching insertion of any USB device");
-	if (!ins && !id) pr_info("Loaded but not functional");
+	if (log){
+		if (id) pr_info("Watching removal of USB device 0x%08x\n", id);
+		if (ins) pr_info("Watching insertion of any USB device");
+		if (!ins && !id) pr_info("Loaded but not functional");
+	}
 	return 0;
 }
 module_init(deadman_init);
 
 static void __exit deadman_exit(void) {
 	usb_unregister_notify(&usb_notify);
-	if (id) pr_info("Stopped watching USB device 0x%08x\n", id);
-	if (ins) pr_info("Stopped watching insertion of USB devices");
-	if (!ins && !id) pr_info("Unloaded");
+	if (log){
+		if (id) pr_info("Stopped watching USB device 0x%08x\n", id);
+		if (ins) pr_info("Stopped watching insertion of USB devices");
+		if (!ins && !id) pr_info("Unloaded");
+	}
 }
 module_exit(deadman_exit);
